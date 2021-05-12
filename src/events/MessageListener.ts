0d7254406596661302ffc3696d19f16032884c66
RootGC <matheusbzo@hotmail.com>
2021-05-12 00:14:25 -0300

    Multiple Errors in Commands

---
+++ src/events/MessageListener.ts
@@ -0,0 +1,25 @@
+import { EventContext } from '../utils'
+import { prefix, owner } from '../config.json'
+import { Message } from 'eris'
+import IllyaClient from '../Client'
+export default class MessageListener extends EventContext {
+    public constructor(client: IllyaClient) {
+        super(client, 'messageCreate')
+    }
+
+    run(message: Message) {
+        if (message.author.bot) return
+        if (message.channel.type !== 0) return
+
+        if (!message.content.startsWith(prefix)) return
+
+        const args: string[] = message.content.slice(prefix.length).trim().split(/ +/g)
+        const cmd: string = args.shift().toLowerCase()
+        const commands = this.client.commands.get(cmd) || this.client.commands.get(this.client.aliases.get(cmd))
+
+        if (!commands) return
+        if (commands.config.dev && !owner.includes(message.author.id)) return
+        message.channel.sendTyping()
+        commands.run(message, args)
+    }
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+import EventContext from './EventContext'
+import CommandContext from './CommandContext'
+import EmbedBuilder from './EmbedBuilder'
+const Colors = require('./Colors.ts')
+export {
+    EventContext,
+    CommandContext,
+    EmbedBuilder,
+    Colors
+}
